9ca0f55a8032b60d34138c855ede770bdda21ccf
Layhout <85871695+Layhout@users.noreply.github.com>
2021-08-25 14:59:05 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwJOGTt7JaT62fafd7FWS2mKW8YnsFBB0",
-    authDomain: "linkedin-clone-365d9.firebaseapp.com",
-    projectId: "linkedin-clone-365d9",
-    storageBucket: "linkedin-clone-365d9.appspot.com",
-    messagingSenderId: "683745168802",
-    appId: "1:683745168802:web:f4a7d461f761f3aaf936fb"
+  apiKey: "AIzaSyB88cHsfXcOyz761QPq0_4oSKOJ9BZ0UlI",
+  authDomain: "test-with-react-5c0ff.firebaseapp.com",
+  databaseURL: "https://test-with-react-5c0ff-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "test-with-react-5c0ff",
+  storageBucket: "test-with-react-5c0ff.appspot.com",
+  messagingSenderId: "251317291067",
+  appId: "1:251317291067:web:d30c4883769b905d83341e"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
